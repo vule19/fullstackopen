65997e6d3ec06fbb6c93a8e5ae767d1d4ea822c9
vule19 <anhvle1901@gmail.com>
2026-01-19 18:54:08 -0500

    9.1-9.3

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (height: number, weight: number) : string => {
+    const bmi = weight / (height * height / 10000)
+
+    if (bmi < 18.5) {
+        return "Under weight"
+    }
+    else if (bmi < 24.9) {
+        return "Normal range"
+    }
+    else {
+        return "Over weight"
+    }
+}
+
+console.log(calculateBmi(180, 74))
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,51 @@
+import { isNotNumber } from "./utils"
+
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface Values {
+    target: number;
+    arr: number[];
+}
+
+const parseArguments = (args: string[]): Values => {
+    if (!isNotNumber(Number(args[2]))) {
+        return {
+            target: Number(args[2]),
+            arr: args.slice(3).map(val => Number(val))
+        }
+    }
+}
+
+const calculateExercises = (hours: number[], target: number) : Result => {   
+    const numberOfDays = hours.length
+    const trainingDays = hours.filter(h => h !== 0).length
+    const average = hours.reduce((hour, curr) => hour + curr, 0) / numberOfDays
+    return {
+        periodLength: numberOfDays,
+        trainingDays: trainingDays,
+        success: average > target,
+        rating: 2,
+        ratingDescription: 'good',
+        target: target,
+        average: average
+    }
+}
+
+try {
+    const {target, arr} = parseArguments(process.argv);
+    console.log(calculateExercises(arr, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
